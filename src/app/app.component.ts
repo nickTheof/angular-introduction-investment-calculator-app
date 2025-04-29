64a08f0b4d8e7a54316f03ae0b3ef5551198264d
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-29 11:09:09 +0300

    feat: add header and user-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { UserInputComponent } from './components/user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'investment-calculator-angular';
